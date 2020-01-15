88f3f1b70f56da0f1ed659f79144ff684e313321
John Dinh <37444633+JohnDinhCodes@users.noreply.github.com>
2020-01-14 19:16:08 -0500

    Updates

---
+++ precache-manifest.520a7ea5264cea11f837f8ac7c09320b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5910ba716c2c8737af0d6db8e6f79f8c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "47d84aa490620fc55054",
+    "url": "/monsters-rolodex/static/css/main.47c90390.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47d84aa490620fc55054",
+    "url": "/monsters-rolodex/static/js/main.5678a8d4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
